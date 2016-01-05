3e767706b53f444813c6a81a4e82dd2258cf7660
Thomas Shivers <tcshivers1970@yahoo.com>
2016-01-05 23:22:58 +0000

    Lesson 35

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,9 @@
 class Topic < ActiveRecord::Base
    has_many :posts
    has_many :sponsored_posts
+   
+   validates :name, length: { minimum: 5 }, presence: true
+   validates :description, length: { minimum: 15 }, presence: true
+   validates :topic, presence: true
+   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151217162006) do
+ActiveRecord::Schema.define(version: 20151222151946) do
 
-  create_table "adevertisements", force: :cascade do |t|
+  create_table "advertisements", force: :cascade do |t|
     t.string   "title"
     t.text     "copy"
     t.integer  "price"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151217162006) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
+    t.string   "title"
     t.text     "body"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
@@ -68,15 +69,12 @@ ActiveRecord::Schema.define(version: 20151217162006) do
 
   add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
 
-  create_table "sponsoreds", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.integer  "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
-
-  add_index "topics", ["sponsored_id"], name: "index_topics_on_sponsored_id"
 
 end
