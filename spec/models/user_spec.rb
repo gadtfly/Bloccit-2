37c275a7601fab0adabb8d14f094914a87728066
Thomas Shivers <tcshivers1970@yahoo.com>
2016-01-28 22:39:16 +0000

    Lesson 36

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,12 @@ RSpec.describe User, type: :model do
      it "should respond to email" do
        expect(user).to respond_to(:email)
      end
+     
+     it "should format the user's name" do
+         user.name = "bloc user"
+         user.save
+         expect(user.name).to eq "Bloc user"
+     end
    end
    # #1
    describe "invalid user" do
